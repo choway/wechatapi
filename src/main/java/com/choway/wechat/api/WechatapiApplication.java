7dddff13e0e199a236008cbf7817a8137a22b61f
choway <354988859@qq.com>
2019-01-15 15:23:22 +0800

    init commit.

---
+++ src/main/java/com/choway/wechat/api/WechatapiApplication.java
@@ -0,0 +1,13 @@
+package com.choway.wechat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatapiApplication.class, args);
+	}
+
+}
